8a627aaecd3a40a5a3c9333f6a2e94f2e23ee467
aisling-lby <aisling.lby@gmail.com>
2026-01-27 07:02:03 +0000

    add compose.yaml

--- hw1/notebook.py
+++ hw1/notebook.py
@@ -10,78 +10,17 @@ from sqlalchemy import create_engine
 
 # In[2]:
 
+def run()
+    taxi_df = pd.read_parquet('https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2025-11.parquet')
+    engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
+    print(pd.io.sql.get_schema(taxi_df, name='green_taxi_data', con=engine))
 
-taxi_df = pd.read_parquet('https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2025-11.parquet')
+    taxi_df.to_sql(name='green_taxi_data', con=engine, if_exists='replace')
 
+    zone_df = pd.read_csv('https://github.com/DataTalksClub/nyc-tlc-data/releases/download/misc/taxi_zone_lookup.csv')
+    print(pd.io.sql.get_schema(zone_df, name='zone', con=engine))
+    zone_df.to_sql(name='zone', con=engine, if_exists='replace')
 
-# In[3]:
-
-
-taxi_df.head()
-
-
-# In[5]:
-
-
-taxi_df.dtypes
-
-
-# In[6]:
-
-
-len(taxi_df)
-
-
-# In[10]:
-
-
-engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
-
-
-# In[11]:
-
-
-print(pd.io.sql.get_schema(taxi_df, name='green_taxi_data', con=engine))
-
-
-# In[15]:
-
-
-taxi_df.to_sql(name='green_taxi_data', con=engine, if_exists='replace')
-
-
-# In[19]:
-
-
-zone_df = pd.read_csv('https://github.com/DataTalksClub/nyc-tlc-data/releases/download/misc/taxi_zone_lookup.csv')
-
-
-# In[20]:
-
-
-len(zone_df)
-
-
-# In[21]:
-
-
-zone_df.head()
-
-
-# In[22]:
-
-
-zone_df.dtypes
-
-
-# In[23]:
-
-
-print(pd.io.sql.get_schema(zone_df, name='zone', con=engine))
-
-
-# In[24]:
-
-
-zone_df.to_sql(name='zone', con=engine, if_exists='replace')
+if __name__ == '__main__':
+    run()
 
